ce771b70ce1f8e8cea1304212d5b591e69cee92b
jboyxs <3408970170@qq.com>
2024-08-17 13:31:54 +0800

    加入pid文件但是还没有实现pid控速

--- smartcar/band1/Peripheral/follow.c
+++ smartcar/band1/Peripheral/follow.c
@@ -9,5 +9,5 @@ void FOLLOW_Start(follow *state)
     state->out3=HAL_GPIO_ReadPin(OUT3_Pin,OUT3_GPIO_Port);
     state->out4=HAL_GPIO_ReadPin(OUT4_Pin,OUT4_GPIO_Port);
     //以下就是根据传感器的状态给速度了
-    if(state.out0)
+   //if(state.out0)
 }
