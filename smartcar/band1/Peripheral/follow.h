db8bdeefb3d455a6a4f1a8c53c79dd81dfeaad02
jboyxs <3408970170@qq.com>
2024-08-17 12:14:22 +0800

    出现提交不上去的问题了

--- smartcar/band1/Peripheral/follow.h
+++ smartcar/band1/Peripheral/follow.h
@@ -3,6 +3,18 @@
 
 #include "main.h"
 #include "stm32f1xx_hal.h"
+typedef struct 
+{
+   uint8_t out0;
+   uint8_t out1;
+   uint8_t out2;
+   uint8_t out3;
+  uint8_t out4;
+   float left_speed;
+   float right_speed;
+    /* data */
+} follow;
+void FOLLOW_Start(follow* state);
 
 
 #endif // FOLLOW_H
