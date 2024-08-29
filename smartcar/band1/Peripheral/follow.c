7e687ed990989051bf3bdffa33456b2386093e31
jboyxs <3408970170@qq.com>
2024-08-29 20:57:24 +0800

    循迹代码写的差不多了（四路循迹，大部分是从网上抄的），下一步就是实际测速调速调控速pid了

--- smartcar/band1/Peripheral/follow.c
+++ smartcar/band1/Peripheral/follow.c
@@ -7,7 +7,63 @@ void FOLLOW_Start(follow *state)
     state->out1=HAL_GPIO_ReadPin(OUT1_GPIO_Port,OUT1_Pin);
     state->out2=HAL_GPIO_ReadPin(OUT1_GPIO_Port,OUT2_Pin);
     state->out3=HAL_GPIO_ReadPin(OUT1_GPIO_Port,OUT3_Pin);
-state->out4=HAL_GPIO_ReadPin(OUT1_GPIO_Port,OUT4_Pin);
+    //state->out4=HAL_GPIO_ReadPin(OUT1_GPIO_Port,OUT4_Pin);
     //以下就是根据传感器的状态给速度了
-   //if(state.out0)
+    //先抄一下网上的四路循迹
+   if(!state->out0&&state->out1&&state->out2&&!state->out3)
+   {
+    //标准的直行，左右速度的值具体还要改
+    //state->left_speed=100;
+    //state->right_speed=100; 
+    state->nowstate=0;
+   }
+    else if(!state->out0&&!state->out1&&state->out2&&!state->out3)
+    {
+        //右小转，左右速度的值具体还要改
+        //state->left_speed=100;
+        //state->right_speed=50;
+        state->nowstate=1;
+    }
+    else if(!state->out0&&!state->out1&&!state->out2&&state->out3)
+    {
+        //右大转，左右速度的值具体还要改
+        //state->left_speed=;
+        //state->right_speed=;
+        state->nowstate=2;
+    }
+    else if(!state->out0&&state->out1&&!state->out2&&!state->out3)
+    {
+        //左小转，左右速度的值具体还要改
+        //state->left_speed=50;
+        //state->right_speed=100;
+        state->nowstate=3;
+    }
+    else if(state->out0&&!state->out1&&!state->out2&&!state->out3)
+    {
+        //左大转，左右速度的值具体还要改
+        //state->left_speed=50;
+        //state->right_speed=50;
+        state->nowstate=4;
+    }
+    if(!state->out0&&!state->out1&&state->out2&&!state->out3)
+    {
+        if(state->laststate==1)
+        {
+            //上一个状态是右小转
+            //右小中转，左右速度的值具体还要改
+            //state->left_speed=100;
+            //state->right_speed=50;
+            state->nowstate=5;
+        }
+        else if(state->laststate==3)
+        {
+            //上一个状态是左小转
+            //左小中转，左右速度的值具体还要改
+            //state->left_speed=50;
+            //state->right_speed=100;
+            state->nowstate=6;
+        }
+    }
+   state->laststate=state->nowstate;
+   
 }
--- smartcar/band1/Peripheral/follow.h
+++ smartcar/band1/Peripheral/follow.h
@@ -9,11 +9,14 @@ typedef struct
    uint8_t out1;
    uint8_t out2;
    uint8_t out3;
-  uint8_t out4;
+    //感觉上面四个状态量是不是存在数组里更合适一点
+    uint8_t nowstate;
+   uint8_t laststate;
    float left_speed;
    float right_speed;
     /* data */
 } follow;
+//到时候这个函数感觉是加到while循环里面合适还是加到控速pid的定时中断服务函数里面合适呢？
 void FOLLOW_Start(follow* state);
 
 
